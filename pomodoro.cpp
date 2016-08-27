4382d9933e3a6d9eac32d6f568b36b3d988694cd
Mahmoud <md2989@nyu.edu>
2016-08-26 22:15:31 -0400

    Fixed bug

--- pomodoro.cpp
+++ pomodoro.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <time.h>
 #include <math.h>
+#include <string>
 
-const int WORK_TIME = 1500;
-const int BREAK_TIME = 240;
-const int LONG_BREAK_TIME = 1200;
-
-const int CHECKS = 4;
+const double WORK_TIME = 1500;
+const double BREAK_TIME = 240;
+const double LONG_BREAK_TIME = 1200;
+const double SEC_IN_MIN = 60;
+const int TOTAL_COUNT = 3;
 
 void PassTime(int timePass){
     time_t timer;
@@ -14,19 +15,18 @@ void PassTime(int timePass){
     time(&start);    //Get start time
 
     if (timePass == WORK_TIME)
-        std::cout << "Work Time" << std::endl;
+        std::cout << "Work Time: " << WORK_TIME/SEC_IN_MIN << " minutes" << std::endl;
     else if (timePass == BREAK_TIME)
-        std::cout << "Short Break Time" << std::endl;
+        std::cout << "Short Break Time" << BREAK_TIME/SEC_IN_MIN << " minutes" << std::endl;
     else if (timePass == LONG_BREAK_TIME)
-        std::cout << "Extended Break Time" << std::endl;
+        std::cout << "Extended Break Time" << LONG_BREAK_TIME/SEC_IN_MIN << " minutes" << std::endl;
 
-    while (true){
+    while (true){   
         time(&timer);
         if (difftime(timer, start) == timePass){
             break;
         }
     }
-    std::cout << "End" << std::endl;
 }
 
 void Cycle(){
@@ -34,15 +34,21 @@ void Cycle(){
     PassTime(BREAK_TIME);
 }
 
+void ClearScreen(){
+    std::cout << std::string(50, '\n');
+}
+
 void StartWork(){
     int count = 0;
     while (true){
-        if (count != CHECKS){
-            Cycle();
-            count++;
+        if (count == TOTAL_COUNT){
+            ClearScreen();
+            PassTime(LONG_BREAK_TIME);
+            count = 0;
         }
         else{
-            PassTime(LONG_BREAK_TIME);
+            Cycle();
+            count++;
         }
     }
 }
